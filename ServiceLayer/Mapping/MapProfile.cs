095ded3adb75dc0413b531e93aa707b6baa92b07
Nebil Aggun <nebil.aggun@gmail.com>
2022-08-12 16:52:10 +0300

    ServiceLayer function added and mapping,validation function

---
+++ ServiceLayer/Mapping/MapProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using CoreLayer.DTOs;
+using CoreLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<ProductUpdateDto, Product>();
+            CreateMap<Product, ProductWithCategoryDto>();
+            CreateMap<Category, CategoryWithProductsDto>();
+        }
+    }
+}
---
+++ ServiceLayer/Services/ProductService.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using CoreLayer.DTOs;
+using CoreLayer.Models;
+using CoreLayer.Repositories;
+using CoreLayer.Services;
+using CoreLayer.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Services
+{
+    public class ProductService : Service<Product>, IProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public ProductService(IGenericRepository<Product> repository, IUnitOfWork unitOfWork, IMapper mapper, IProductRepository productRepository) : base(repository, unitOfWork)
+        {
+            _mapper = mapper;
+            _productRepository = productRepository;
+        }
+
+        public async Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetProductsWithCategory()
+        {
+            var products = await _productRepository.GetProductsWitCategory();
+
+            var productsDto = _mapper.Map<List<ProductWithCategoryDto>>(products);
+            return CustomResponseDto<List<ProductWithCategoryDto>>.Success(200, productsDto);
+        }
+    }
+}
